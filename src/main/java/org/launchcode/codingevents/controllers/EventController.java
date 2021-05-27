725fb4c309bd0d8d8ccb36ef3c4cb2e22b8f7531
Matt Plog <ps3hawk@gmail.com>
2021-05-27 11:18:01 -0500

    starter

--- src/main/java/org/launchcode/codingevents/controllers/EventController.java
+++ src/main/java/org/launchcode/codingevents/controllers/EventController.java
@@ -2,10 +2,7 @@ package org.launchcode.codingevents.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -36,6 +33,13 @@ public class EventController {
     public String processCreateEventForm(@RequestParam String eventName) {
         events.add(eventName);
         return "redirect:";
+    }
+    public String displayEditForm(Model model, @PathVariable int eventId) {
+        // controller code will go here
+    }
+
+    public String processEditForm(int eventId, String name, String description) {
+        // controller code will go here
     }
 
 }
