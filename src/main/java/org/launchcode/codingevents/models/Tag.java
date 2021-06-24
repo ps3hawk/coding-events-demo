e047d762a47233d57c9525ee8987e7717b9962b6
Matt Plog <ps3hawk@gmail.com>
2021-06-24 16:17:45 -0500

    18-6-finished

--- src/main/java/org/launchcode/codingevents/models/Tag.java
+++ src/main/java/org/launchcode/codingevents/models/Tag.java
@@ -7,15 +7,16 @@ import javax.validation.constraints.Size;
 import java.util.ArrayList;
 import java.util.List;
 
-/**
- * Created by Chris Bay
- */
+
 @Entity
 public class Tag extends AbstractEntity {
 
     @Size(min = 1, max = 25)
     @NotBlank
     private String name;
+
+    @ManyToMany(mappedBy = "tags")
+    private final List<Event> events = new ArrayList<>();
 
     public Tag(String name) {
         this.name = name;
@@ -35,4 +36,7 @@ public class Tag extends AbstractEntity {
         this.name = name;
     }
 
+    public List<Event> getEvents() {
+        return events;
+    }
 }
